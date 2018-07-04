4a871b1be1ef79d9838b7df8e496ffb5d38c2f17
Rob Ross <rm7677@gmail.com>
2018-07-04 12:43:51 -0400

    render a list of clients

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clients: [
+        { id: 1, name: 'Tommy Preston' },
+        { id: 2, name: 'Jamison Abbott' },
+        { id: 3, name: 'Aida Mccoy' },
+        { id: 4, name: 'Lawrence Duffy' },
+        { id: 5, name: 'Omer Salinas' },
+        { id: 6, name: 'Phyllis Bass' },
+        { id: 7, name: 'Lane Harrell' },
+        { id: 8, name: 'Kristopher Weiss' },
+        { id: 9, name: 'Aileen Solis' },
+        { id: 10, name: 'Mark Krueger' },
+        { id: 11, name: 'Sofia Rhodes' },
+        { id: 12, name: 'Allan Savage' }
+      ],
+      selectedClient: null
+    };
+  }
+
   render() {
+    const clientListItems = this.state.clients.map(c => <li key={c.id}>{c.name}</li>);
+
     return (
-      <h1>Chip Fitness</h1>
+      <div>
+        <h1>Clients</h1>
+        <ul className="list-clients">
+          {clientListItems}
+        </ul>
+      </div>
     );
   }
 }
